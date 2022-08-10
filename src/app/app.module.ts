15139898941b7a1347de384beabbbea654e891f2
unknown <harini.lekkala1997@gmail.com>
2022-08-11 00:58:27 +0530

    [Harini]: rigisterpage , signin page , forgot page , reset page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SigninComponent } from './Components/signin/signin.component';
+import { ForgotpageComponent } from './forgotpage/forgotpage.component';
+import { ResetpageComponent } from './resetpage/resetpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    SigninComponent,
+    ForgotpageComponent,
+    ResetpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/forgotpage/forgotpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgotpage',
+  templateUrl: './forgotpage.component.html',
+  styleUrls: ['./forgotpage.component.scss']
+})
+export class ForgotpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
